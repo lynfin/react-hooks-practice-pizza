1f51bbd1b32b76d2ae75db64fc1cf94ab331a9c5
Lyn <lynfinman@gmail.com>
2022-06-15 11:08:21 -0400

    displaying list of pizzas

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
+// ┌ └ ─ ├ │
+// App [pizzas]
+//  ├ Header
+//  ├ PizzaForm
+//  └ PizzaList
+//      └ Pizza
+
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+      .then((r) => r.json())
+      .then((pizzas) => setPizzas(pizzas));
+  }, []);
+  console.log("Pizza count", pizzas.length);
   return (
     <>
       <Header />
       <PizzaForm />
-      <PizzaList />
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Pizza from "./Pizza";
 
-function PizzaList() {
+function PizzaList({ pizzas }) {
   return (
     <table className="table table-striped">
       <thead>
@@ -13,9 +13,9 @@ function PizzaList() {
         </tr>
       </thead>
       <tbody>
-        {
-          //render Pizza here
-        }
+        {pizzas.map((pizza) => (
+          <Pizza pizza={pizza} key={pizza.id} />
+        ))}
       </tbody>
     </table>
   );
